51e7637487f01e4a54c2e1ac3ac6f882f1e9823f
Steven Adam <s.adam0930@gmail.com>
2017-11-05 12:12:31 -0500

    profile for created_on

---
+++ CoinMapDataProfiler.java
@@ -0,0 +1,79 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.IOException;
+
+public class CoinMapDataProfiler {
+
+    public class CoinMapDataMapper extends Mapper<LongWritable, Text, Text, Text> {
+        // Columns of interest:
+        private final int CATEGORY = 0;
+        private final int CITY = 1;
+        private final int COUNTRY = 2;
+        private final int CREATED_ON = 3;
+        private final int NAME = 12;
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            if(key != 0) { //first line is column header
+                String line = value.toString();
+                String columns[] = line.split(",", -1);
+                Text outputKey = new Text("created_on");
+                Text outputValue = new Text(columns[CREATED_ON]);
+                context.write(outputKey, outputValue);
+            }
+        }
+    }
+
+    public class CoinMapDataReducer extends Reducer<Text, Text, Text, Text> {
+        
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            int min = Integer.MIN_VALUE;
+            int max = Integer.MAX_VALUE;
+
+            for(Text value : values) {
+                int v = Integer.parseInt(value.get());
+                if(v > min) {
+                    min = v;
+                }
+                if(v < max) {
+                    max = v;
+                }
+            }
+            
+            context.write(new Text("min created_on"), new IntWritable(min));
+            context.write(new Text("max created_on"), new IntWritable(max));
+        }
+    }
+
+    public static void main (String[] args) throws Exception {
+        if (args.length < 2){
+            System.err.println("Usage: CoinMapDataProfiler <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = new Job();
+        job.setJarByClass(CoinMapDataProfiler.class);
+        job.setJobName("CoinMap Data Profiler");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setNumReduceTasks(1); // 1 Reduce task 
+        
+        job.setMapperClass(CoinMapDataMapper.class);
+        job.setReducerClass(CoinMapDataReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
