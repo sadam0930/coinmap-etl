a71c6e52a68ca1e4fd6ec602cf87140827daffbf
Steven Adam <s.adam0930@gmail.com>
2017-11-05 15:49:56 -0500

    update preprocessing for commas in city names

--- preprocess.py
+++ preprocess.py
@@ -2,4 +2,4 @@ import csv
 with open("bitvenues.csv", "rb") as input, open("bitvenues2.csv", "wb") as output:
     w = csv.writer(output)
     for record in csv.reader(input):
-        w.writerow(tuple(s.replace('\n', ' ') for s in record))
+        w.writerow(tuple(s.replace('\n', ' ').replace(',','') for s in record))
